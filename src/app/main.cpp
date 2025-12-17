655d1dfa9bdbd03d367354d96e82eda62c617a80
AXRay <vandpal@bk.ru>
2025-12-17 17:11:27 +0300

    Точка входа

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,34 +4,6 @@
 #include "../../include/math/Math.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML Example");
-    window.setFramerateLimit(60);
-
-    
-    gmath::Vector3f vec(1.0f, 2.0f, 3.0f);
-    std::cout << vec << std::endl;
-    gmath::Vector3f a(1.0f, 2.0f, 3.0f);
-    std::cout << a << std::endl;
-
-    gmath::Matrix3f m();    
-    
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close(); 
-            }
-            
-            /*Здесь же обработка нажатий*/
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
 
     return 0;
 }
