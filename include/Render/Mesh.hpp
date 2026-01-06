66d9c35d4e3adc88293cf324c065ad95bdc0f14b
ViperEvil <shulzhenko.23@mail.ru>
2026-01-06 17:05:57 +0300

    f

--- include/Render/Mesh.hpp
+++ include/Render/Mesh.hpp
@@ -7,18 +7,13 @@
 #include "Math/Vector3.hpp"
 #include <vector>
 
-struct Vertex {
-    gmath::Vector3<double> position;
-    gmath::Vector3<double> normal;
-};
-
 // TODO: добавить поле атрибута для цвета? UV-коорды (на потом)
 // TODO: вопрос триангуляции в случае, когда полигоны != треугольники
 class Mesh {
     public:
-        std::vector<Vertex> m_vertices;
-        //std::vector<gmath::Vector3<double>> m_normals;
-        //std::vector<int> m_indices;
+        std::vector<gmath::Vector3<double>> m_vertices;
+        std::vector<gmath::Vector3<double>> m_normals;
+        // std::vector<int> m_indices;
         std::vector<std::vector<int>> m_faces;
 
         Mesh() = default;
@@ -27,13 +22,7 @@ class Mesh {
             const std::vector<gmath::Vector3<double>>& normals,
             //const std::vector<int>& indices,
             const std::vector<std::vector<int>>& faces
-            ) : m_faces(faces) {
-            for (size_t i = 0; i < vertices.size(); i++) {
-                gmath::Vector3<double> normal = (i < normals.size())
-                    ? normals[i] : gmath::Vector3<double>{0, 0, 0};
-                m_vertices.push_back({vertices[i], normal});
-            }
-        }
+            ) : m_faces(faces) {}
         ~Mesh() = default;
 
         int vertex_count() const { return m_vertices.size(); }
