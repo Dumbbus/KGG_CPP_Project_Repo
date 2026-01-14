963881683e7cb3d07fb9496bc2877f2d00273ce8
ViperEvil <shulzhenko.23@mail.ru>
2026-01-14 16:32:08 +0300

    Косяк, в общем в SFML есть своя реализация загрузчика

--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -192,7 +192,7 @@ void Window::create_Window() {
                     is_flat_shading,
                     light_direction,
                     ambient = 1.0f,
-                    Color::yellow()
+                    {255, 0, 0, 120}
                     );
 
                 // rasterizer.draw_wireframe(
