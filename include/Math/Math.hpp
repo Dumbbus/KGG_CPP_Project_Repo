8269b4bab89f88377f32f398ffdf4ed2992c7bfa
AXRay <vandpal@bk.ru>
2026-01-16 23:21:51 +0300

    Add files via upload

---
+++ include/Math/Math.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../Math/Matrix3.hpp"
+#include "../Math/Matrix4.hpp"
+#include "../Math/Vector2.hpp"
+#include "../Math/Vector3.hpp"
+#include "../Math/Vector4.hpp"
+#include "../Math/MConcepts.hpp"
